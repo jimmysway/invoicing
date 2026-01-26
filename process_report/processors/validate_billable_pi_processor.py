5d5e57d79a01d8f46c71f60c474a92dd40030dbc
Quan Pham <qmpham2019@gmail.com>
2026-01-26 14:02:33 -0800

    Added `barcelona` as nonbillable cluster

--- process_report/processors/validate_billable_pi_processor.py
+++ process_report/processors/validate_billable_pi_processor.py
@@ -11,16 +11,18 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-NONBILLABLE_CLUSTERS = ["ocp-test"]
+NONBILLABLE_CLUSTERS = ["ocp-test", "barcelona"]
 
 
 @dataclass
 class ValidateBillablePIsProcessor(processor.Processor):
     """
     This processor validates the billable PIs and projects in the data,
-    and determines if a project is billable or not.
+    and determines if a project is billable or not using several criterias:
 
-    Every project belonging to ocp-test is nonbillable.
+    - The PI is nonbillable
+    - The project (identified by project name) is nonbillable
+    - The project belongs in `NONBILLABLE_CLUSTERS`
     """
 
     nonbillable_pis: list[str] = field(default_factory=loader.get_nonbillable_pis)
